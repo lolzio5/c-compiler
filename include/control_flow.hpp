50f98aa6aa0b600d41fa2799e45d091f91067abc
lolzio5 <loleziovioramarquet@gmail.com>
2024-03-21 14:49:33 +0000

    if else then statements added (18/68)

--- include/control_flow.hpp
+++ include/control_flow.hpp
@@ -36,5 +36,46 @@ public:
         stream<<"}"<<std::endl;
     }
 };
-#endif
 
+class IfElseStatement : public Node
+{
+private:
+    Node* condition;
+    Node* if_statement;
+    Node* else_statement;
+public:
+    IfElseStatement(Node* condition_, Node* if_statement_, Node* else_statement_) : condition(condition_), if_statement(if_statement_), else_statement(else_statement_) {}
+    ~IfElseStatement(){
+        delete condition;
+        delete if_statement;
+        delete else_statement;
+    }
+
+    void EmitRISC(std::ostream &stream, Context &context, int destReg) const {
+        int conditionValueRegister = context.findFreeRegister();
+
+        condition->EmitRISC(stream, context, conditionValueRegister);
+        std::string falseBranch=context.nameNewBranch();
+
+        std::string continueBranch=context.nameNewBranch();
+
+        stream<<"beq "<<context.getRegisterName(conditionValueRegister)<<", zero, "<<falseBranch<<std::endl;
+        if_statement->EmitRISC(stream, context, destReg);
+        stream<<"j "<<continueBranch<<std::endl;
+        stream<<falseBranch<<":"<<std::endl;
+        else_statement->EmitRISC(stream, context, destReg);
+        stream<<continueBranch<<":"<<std::endl;
+        context.freeRegister(conditionValueRegister);
+    }
+    void Print(std::ostream &stream) const {
+        stream<<"if(";
+        condition->Print(stream);
+        stream<<"){";
+        if_statement->Print(stream);
+        stream<<"}"<<std::endl;
+        stream<<"else {";
+        else_statement->Print(stream);
+        stream<<"}"<<std::endl;
+    }
+};
+#endif
